70771bed20a95a0b4659e4d63769b765059a12dd
kaviraj2003 <2112064@nec.edu.in>
2024-01-17 17:45:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MouthDetection from './mouthdetector';
+
+function App() {
+  return (
+    <div>
+        <MouthDetection/>
+    </div>
+  );
+}
+
+export default App;
